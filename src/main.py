e8858a20e5b6149235711718994d5e8aae30407a
mnsavag <mnsavag@gmail.com>
2023-11-25 18:49:14 +0500

    mvp

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.responses import Response
-from src.utils.alias_generators import to_snake, to_camel, to_pascal
 from src.api.v1.api import api_router
-from starlette.datastructures import MutableHeaders
-import json
 
 
 app = FastAPI()
@@ -15,13 +12,13 @@ app = FastAPI()
 async def lifespan(app: FastAPI):
     yield
 
-#async def catch_exceptions_middleware(request: Request, call_next):
-#    try:
-#        return await call_next(request)
-#    except Exception:
-#        raise Response("Internal Server Error", status_code=500)
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception:
+        raise Response("Internal Server Error", status_code=500)
 
 
-#app.middleware('http')(catch_exceptions_middleware)
+app.middleware('http')(catch_exceptions_middleware)
 
 app.include_router(api_router)
